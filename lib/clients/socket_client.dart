fee6b095cd6318c971a75d9d0f3f3016d7cd355e
razken-hash <ka_kenniche@esi.dz>
2024-05-24 20:45:27 +0100

    SocketClientIO Added

---
+++ lib/clients/socket_client.dart
@@ -0,0 +1,24 @@
+import 'package:socket_io_client/socket_io_client.dart' as sockets;
+import 'package:tera_app/utils/constants.dart';
+
+class SocketClient {
+  sockets.Socket? socket;
+
+  static SocketClient? _instance;
+
+  SocketClient._internal() {
+    socket = sockets.io(
+      Uri.parse(BASE_URL),
+      <String, dynamic>{
+        "transports": ["websocket"],
+        "autoConnect": false,
+      },
+    );
+    socket!.connect();
+  }
+
+  static SocketClient get getInstance {
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
+}
